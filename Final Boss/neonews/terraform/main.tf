db8d8d54d089254dff66fb481419af9a73d34b7a
StefanDima14 <stefand@E1677230.internal.vodafone.com>
2026-01-14 16:35:56 +0200

    enable cloudwatch agent and iam polciies

--- Final Boss/neonews/terraform/main.tf
+++ Final Boss/neonews/terraform/main.tf
@@ -137,6 +137,16 @@ resource "aws_iam_role_policy_attachment" "s3_full" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
+resource "aws_iam_role_policy_attachment" "s3_full" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "s3_full" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SSMFullAccess"
+}
+
 # EC2 Instance Profile
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "EC2-Instance-Profile"
