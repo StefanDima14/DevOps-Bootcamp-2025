7bea8bf9db9027811a6ac07744f4bb83eacb0e4c
StefanDima14 <stefand@E1677230.internal.vodafone.com>
2026-01-18 21:35:29 +0200

    attach new role for ec2

--- Final Boss/neonews/terraform/main.tf
+++ Final Boss/neonews/terraform/main.tf
@@ -147,7 +147,7 @@ resource "aws_iam_role_policy_attachment" "ssm_full" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "ssm_full" {
+resource "aws_iam_role_policy_attachment" "agent_policy" {
   role       = aws_iam_role.ec2_ecr_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
