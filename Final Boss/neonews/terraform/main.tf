c4d836c9b13e656620f84bf96b664d5786478043
StefanDima14 <stefand@E1677230.internal.vodafone.com>
2026-01-18 22:41:24 +0200

    add sg to allow port 8081

--- Final Boss/neonews/terraform/main.tf
+++ Final Boss/neonews/terraform/main.tf
@@ -89,6 +89,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_8080_port" {
   ip_protocol      = "tcp"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_8081_port" {
+  security_group_id = aws_security_group.allow_ssh.id
+  cidr_ipv4      = "0.0.0.0/0"
+  from_port        = 8081
+  to_port          = 8081
+  ip_protocol      = "tcp"
+}
+
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.allow_ssh.id
   cidr_ipv4      = "0.0.0.0/0"
